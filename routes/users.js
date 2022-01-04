99068a88cc8751ba3c70c06c341d987172f87506
Rizky Dwi Saputra <rizky.dwi9900@gmail.com>
2022-01-04 19:38:37 +0700

    Update users.js

--- routes/users.js
+++ routes/users.js
@@ -26,7 +26,7 @@ router.route('/add').post((req, res) => {
    });
 
    newUser.save()
-      .then(() => res.json('User added'))
+      .then(() => res.status(200).json({ "Login" : "Success" }))
       .catch(err => res.status(400).json('Error: ' +err));
 });
 
